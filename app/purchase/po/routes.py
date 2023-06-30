a6ab27a30944e6a7eb83337f0ac6023a4497e89a
n <n@pop-os.localdomain>
2023-06-29 18:57:24 -0600

    added po lines to po details page

--- app/purchase/po/routes.py
+++ app/purchase/po/routes.py
@@ -24,10 +24,18 @@ def get_po(id):
     cursor.execute("SELECT * FROM po WHERE id = %s", (id,))
     po = cursor.fetchone()
 
+    cursor.execute("SELECT po_line.id, po_line.po_id, po_line.product_id, \
+                   product.name, po_line.quantity, product.price \
+                   FROM po_line JOIN product ON po_line.product_id = product.id \
+                   WHERE po_line.po_id = %s", (id,))
+
+    po_lines = cursor.fetchall()
+    total = sum(po_line[5] * po_line[4] for po_line in po_lines)
+
     cursor.close()
     conn.close()
 
-    return render_template('po_detail.html', po=po)
+    return render_template('po_detail.html', po=po, po_lines=po_lines, total=total)
 
 @bp.route('/purchase/pos/')
 def redirect_to_products():
