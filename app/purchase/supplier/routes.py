4389a303df2987ec9ebdc07bd40c323733a85e7d
n <n@pop-os.localdomain>
2023-07-03 21:40:12 -0600

    new supplier

--- app/purchase/supplier/routes.py
+++ app/purchase/supplier/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, request, url_for
 from app.db import connect
 
 bp = Blueprint('supplier', __name__, template_folder='templates')
@@ -32,3 +32,25 @@ def get_supplier(id):
 @bp.route('/purchase/suppliers/')
 def redirect_to_products():
     return redirect(url_for('supplier.get_suppliers'))
+
+@bp.route('/purchase/suppliers/create', methods=['GET', 'POST'])
+def create_supplier():
+    if request.method == 'POST':
+        # Process the form submission and create a new supplier
+        name = request.form.get('name')
+        email = request.form.get('email')
+
+        conn = connect()
+        cursor = conn.cursor()
+
+        # Insert the new supplier into the database
+        cursor.execute("INSERT INTO supplier (name, email) VALUES (%s, %s)", (name, email))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        # Redirect to the supplier list page
+        return redirect(url_for('supplier.get_suppliers'))
+
+    return render_template('create_supplier.html')
