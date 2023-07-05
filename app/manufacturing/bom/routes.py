02c6cdb9cb33fdbe4cbc764533e8c2a39806c777
n <n@pop-os.localdomain>
2023-07-05 14:04:09 -0600

    create bom and bom_lines

--- app/manufacturing/bom/routes.py
+++ app/manufacturing/bom/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, request, url_for
 from app.db import connect
 
 bp = Blueprint('bom', __name__, template_folder='templates')
@@ -35,11 +35,80 @@ def get_bom(id):
     bom_lines = cursor.fetchall()
     components_cost = sum(bom_line[5] * bom_line[4] for bom_line in bom_lines)
 
+    cursor.execute("SELECT id, name FROM product")
+    products = cursor.fetchall()
+
     cursor.close()
     conn.close()
 
-    return render_template('bom_detail.html', bom=bom, bom_lines=bom_lines, components_cost=components_cost)
+    return render_template('bom_detail.html', 
+                           bom=bom, 
+                           bom_lines=bom_lines, 
+                           components_cost=components_cost, 
+                           products=products)
 
 @bp.route('/manufacturing/boms/')
 def redirect_to_products():
     return redirect(url_for('bom.get_boms'))
+
+@bp.route('/manufacturing/boms/create', methods=['GET', 'POST'])
+def create_bom():
+    if request.method == 'POST':
+        # Retrieve product from form
+        product_id = request.form.get('product_id')
+
+        conn = connect()
+        cursor = conn.cursor()
+
+        # Insert new BOM into the database
+        cursor.execute("INSERT INTO bom (product_id) VALUES (%s) RETURNING id", (product_id,))
+
+        bom_id = cursor.fetchone()[0]
+
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('bom.get_bom', id=bom_id))
+    
+    # Retrieve the list of products as soon as /create route is accessed
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, name FROM product")
+    products = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template('bom_create.html', products=products)
+
+@bp.route('/manufacturing/boms/<int:id>/add_bom_line', methods=['POST'])
+def add_bom_line(id):
+    if request.method == 'POST':
+        component_id = request.form.get('component_id')
+        quantity = request.form.get('quantity')
+
+        # Retrieve the BOM based on the provided ID
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM bom WHERE id = %s", (id,))
+        bom = cursor.fetchone()
+
+        if bom is None:
+            # Handle the case where the PO is not found
+            # ...
+
+            cursor.close()
+            conn.close()
+            return redirect(url_for('bom.get_boms'))
+
+        # Insert the BoM line into the database
+        cursor.execute("INSERT INTO bom_line (bom_id, component_id, quantity) VALUES (%s, %s, %s)",
+                       (id, component_id, quantity))
+
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+    return redirect(url_for('bom.get_bom', id=id))
